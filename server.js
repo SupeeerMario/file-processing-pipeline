b021b781c23c1e1fd26fe62a8417acc0c8ef7a2c
supeeermario <khaledshaaban926@gmail.com>
2026-08-28 13:29:18 +0300

    feat: added put and get to storage.js, put builds the params object per call and going through Upload from lib-storage, get sends GetObjectCommand and returns res.Body, and switched the module to an object export with server.js requiring storage and calling storage.ensureBucket

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const connectDB = require('./connectDB');
-const ensureBucket = require('./storage');
+const storage = require('./storage');
 const app = express();
 const port = process.env.SERVER_PORT;
 
-connectDB().then(ensureBucket).catch(err =>{
+connectDB().then(storage.ensureBucket).catch(err =>{
     console.log('error while creating bucket', err);
     process.exit(1);
 }).then(()=> app.listen(port, '0.0.0.0' , ()=>{
--- storage.js
+++ storage.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const awsSdk = require('@aws-sdk/client-s3')
+const awsSdk_Client = require('@aws-sdk/client-s3');
+const awsSdk_Lib = require('@aws-sdk/lib-storage');
 
 const username = process.env.MINIO_ROOT_USER;
 const password = process.env.MINIO_ROOT_PASSWORD;
 
-const s3Client = new awsSdk.S3Client({
+const s3Client = new awsSdk_Client.S3Client({
     region: 'us-east-1',
     endpoint: 'http://minio:9000',
     credentials: {accessKeyId: username, secretAccessKey: password},
@@ -14,7 +15,7 @@ const s3Client = new awsSdk.S3Client({
 
 const BucketName = 'coolbucketname';
 
-const command = new awsSdk.CreateBucketCommand({
+const command = new awsSdk_Client.CreateBucketCommand({
     Bucket: BucketName
 });
 
@@ -37,4 +38,28 @@ async function ensureBucket(){
     }
 };
 
-module.exports = ensureBucket
+async function put(key, stream){
+
+    const uploader = new awsSdk_Lib.Upload({
+        client: s3Client,
+        params: {Bucket: BucketName, Key: key, Body: stream}
+    });
+
+    await uploader.done()
+
+}
+
+async function get(key){
+    
+    const command = new awsSdk_Client.GetObjectCommand({
+        Bucket: BucketName, Key: key
+    });
+    
+    const res = await s3Client.send(command);
+    return res.Body;
+
+}
+
+
+
+module.exports = {ensureBucket, put, get}
